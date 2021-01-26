11a6231f059917907d5e6e611261e38ae4ec8334
F-ARobert <felix-antoine.robert.1@etsmtl.net>
2021-01-26 11:52:54 -0500

    Get size of zones with asserts

--- Lab1/extractProjZone.m
+++ Lab1/extractProjZone.m
@@ -8,7 +8,6 @@ function [database] = extractProjZone(img,shape_image,shape_zones )
 % Ouputs:
 %       database: Image caracteristics vector
 
-
 %%
 % First: Need to reshape image to specified dimensions
 img = imresize(img, shape_image, 'bilinear');
@@ -16,7 +15,44 @@ img = imresize(img, shape_image, 'bilinear');
 % colormap( gray );
 % imagesc( img );
 %%
+% Get Zone array dimensions
+z_rows = uint16(round(shape_image(1)/shape_zones(1), 0));
+% Tests for right shape
+assert(isa(z_rows,'uint16'), 'z_rows is not an unsigned integer');
 
+if shape_image(1) == 60
+    if shape_zones(1) == 5
+        assert((z_rows == 12),'z_rows not equal 12');
+    elseif shape_zones(1) == 10
+        assert((z_rows == 6),'z_rows not equal 6');
+    end
+elseif shape_image(1) == 100
+    if shape_zones(1) == 5
+        assert((z_rows == 20),'z_rows not equal 20');
+    elseif shape_zones(1) == 10
+        assert((z_rows == 10),'z_rows not equal 10');
+    end
+else
+    assert(1==0,'Other error occured in z_rows');
+end
 
+z_cols = uint16(round(shape_image(2)/shape_zones(2),0));
+% Tests for right shape
+assert(isa(z_cols,'uint16'), 'z_cols is not an unsigned integer');
+if shape_image(2) == 50
+    if shape_zones(2) == 5
+        assert((z_cols == 10), 'z_cols not equal 10');
+    elseif shape_zones(2) == 10
+        assert((z_cols == 5),'z_cols not equal 5');
+    end
+elseif shape_image(2) == 80
+    if shape_zones(2) == 5
+        assert((z_cols == 16),'z_cols not equal 16');
+    elseif shape_zones(2) == 10
+        assert((z_cols == 8),'z_cols not equal 8');
+    end
+else
+    assert(1==0,'Other error occured in z_cols')
+end
 
 end
