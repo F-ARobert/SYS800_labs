d44711c7e024f8464e3313303718e979963c76a6
F-ARobert <felix-antoine.robert.1@etsmtl.net>
2021-01-27 11:50:13 -0500

    Renamed variable

--- Lab1/extractProjZone.m
+++ Lab1/extractProjZone.m
@@ -16,25 +16,27 @@ img = round(imresize(img, shape_image, 'bilinear'));
 % imagesc( img );
 %%
 % Get empty Zone array (to hole compressed image)
-database = get_zone_array(shape_image, shape_zones);
+img_zoned = get_zone_array(shape_image, shape_zones);
 
 %%
 % Process each zone in the image
 
 % Split image into sub arrays
-img_split = mat2cell(img, repelem(shape_zones(1), size(database,1)), ...
-        repelem(shape_zones(2), size(database,2)));
+img_split = mat2cell(img, repelem(shape_zones(1), size(img_zoned,1)), ...
+        repelem(shape_zones(2), size(img_zoned,2)));
 
 for i = 1:size(img_split,1)
-    for j = 1:size(img_split,2);
+    for j = 1:size(img_split,2)
         h = sum(img_split{i,j});
         h = int16(h);
-        database(i,j) = mean(h);
+        img_zoned(i,j) = mean(h);
     end
 end
 
-% Uncomment below to visualize database
+% Uncomment below to visualize img_zoned
 colormap( gray );
-imagesc( database );
+imagesc( img_zoned );
+%%
+% Vectorize
 
 end
