d49db8ecd187c8d46f5fac701e98492789eb5e26
F-ARobert <felix-antoine.robert.1@etsmtl.net>
2021-02-01 16:52:52 -0500

    Add loop to generate all databases for zone projection

--- Lab1/main.m
+++ Lab1/main.m
@@ -26,16 +26,23 @@ train_label = mnisttrain(:, 1);
 %         Zone projection method
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 method = 'ZoneProject';
-% parameters: 
-% Zone projection parameters
-shape_image = [60 50];
-shape_zones = [5 5];
-% I) Feature extraction using ZONE
-[database] = make_database(train_data, method, shape_image, shape_zones);
-% Save the features in .mat file
-save(['size_' num2str(shape_image(1)) 'x' num2str(shape_image(2)) ...
-    '_zone_' num2str(shape_zones(1)) 'x' num2str(shape_zones(2)) ...
-    '_learning'], 'database', 'train_label');
+% parameters:
+all_img_sizes = [60 50; 100 80];
+all_zone_sizes = [5 5; 10 10];
+
+for i = 1:size(all_img_sizes(1))+1
+    for j = 1:size(all_zone_sizes(1))+1
+        % Zone projection parameters
+        shape_image = all_img_sizes(i,:);
+        shape_zones = all_zone_sizes(j,:);
+        % I) Feature extraction using ZONE
+        [database] = make_database(train_data, method, shape_image, shape_zones);
+        % Save the features in .mat file
+        save(['size_' num2str(shape_image(1)) 'x' num2str(shape_image(2)) ...
+            '_zone_' num2str(shape_zones(1)) 'x' num2str(shape_zones(2)) ...
+            '_learning'], 'database', 'train_label');
+    end
+end
 
 %% Comment this section if the zone projection method is used
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
