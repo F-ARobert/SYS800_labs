7686b23e7bc76a166c3ec90425d9bb5a052bf142
F-ARobert <felix-antoine.robert.1@etsmtl.net>
2021-02-05 16:28:33 -0500

    uncomment overlap section

--- Lab1/main.m
+++ Lab1/main.m
@@ -46,33 +46,33 @@ train_label = mnisttrain(:, 1);
 %             '_learning'], 'database', 'train_label');
 %     end
 % end
-% %%
-% number_items_in_vector = zeros(1, size(all_img_sizes,1) ... 
-%     *size(all_zone_sizes,1));
-% overlaps = zeros(1, size(all_img_sizes,1) ... 
-%     *size(all_zone_sizes,1));
-% length_index = 1;
-% 
-% for i = 1:size(all_img_sizes(1))
-%     for j = 1:size(all_zone_sizes(1))
-%         % Zone projection parameters
-%         shape_image = all_img_sizes(i,:);
-%         shape_zones = all_zone_sizes(j,:);
-%         
-%         % Load dataset
-%         load(['size_' num2str(shape_image(1)) 'x' num2str(shape_image(2)) ...
-%             '_zone_' num2str(shape_zones(1)) 'x' num2str(shape_zones(2)) ...
-%             '_learning.mat'])
-%         
-%         % Get length of vector F
-%         number_items_in_vector(length_index) = size(database,2);
-%         
-%         % Calculate overlap
-%         %chevauchement(database, train_label)
-%         overlaps(length_index) = chevauchement(database, train_label);
-%         length_index = length_index +1;
-%     end
-% end
+%%
+number_items_in_vector = zeros(1, size(all_img_sizes,1) ... 
+    *size(all_zone_sizes,1));
+overlaps = zeros(1, size(all_img_sizes,1) ... 
+    *size(all_zone_sizes,1));
+length_index = 1;
+
+for i = 1:size(all_img_sizes(1))
+    for j = 1:size(all_zone_sizes(1))
+        % Zone projection parameters
+        shape_image = all_img_sizes(i,:);
+        shape_zones = all_zone_sizes(j,:);
+        
+        % Load dataset
+        load(['size_' num2str(shape_image(1)) 'x' num2str(shape_image(2)) ...
+            '_zone_' num2str(shape_zones(1)) 'x' num2str(shape_zones(2)) ...
+            '_learning.mat'])
+        
+        % Get length of vector F
+        number_items_in_vector(length_index) = size(database,2);
+        
+        % Calculate overlap
+        %chevauchement(database, train_label)
+        overlaps(length_index) = chevauchement(database, train_label);
+        length_index = length_index +1;
+    end
+end
 
 %% Comment this section if the zone projection method is used
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
