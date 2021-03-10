f7d9291e74c72afb7588760c0b91f01b818a0c2d
F-ARobert <felix-antoine.robert.1@etsmtl.net>
2021-03-10 09:04:12 -0500

    Add comments

--- Lab1/main_lab2.m
+++ Lab1/main_lab2.m
@@ -8,11 +8,12 @@ clc;
 
 %%
 % Random Forest hyperparameter testing
-
+% Zone Project method
 method = 'ZoneProject';
 
 [model, results] = RandomForest(method);
-
+%%
+% LBP method
 method = 'LBP';
 
 [model, results] = RandomForest(method);
